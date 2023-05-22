28e96c28c9c61503fdf705ff4800992e6dc172f7
Martin Reinhardt <contact@martinreinhardt-online.de>
2023-05-22 04:55:51 +0200

    chore: Ignore build folder

--- component-library-angular/build/es5/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-// DIRECTIVES
-export { AwesomeCounter, MyComponent } from './directives/proxies';
-// PACKAGE MODULE
-export { ComponentLibraryModule } from './component-library-module';
